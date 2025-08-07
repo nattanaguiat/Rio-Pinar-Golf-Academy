d21e5b87a659a3e74af2a7f37cf94fa5c75d6288
Nattan Aguia <nattan23@msn.com>
2025-08-07 17:28:29 -0400

    add of assets of the app, add of the pages of the app

---
+++ frontend/src/App.jsx
@@ -0,0 +1,28 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Contact from "./pages/Contact";
+import About from "./pages/About";
+import Coaches from "./pages/Coaches";
+import MyBookings from "./pages/MyBookings";
+import MyProfile from "./pages/MyProfile";
+import Booking from "./pages/Booking";
+
+function App() {
+  return (
+    <div className="mx-4 sm:mx[10%]">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/coaches" element={<Coaches />} />
+        <Route path="/booking/:coachID" element={<Booking />} />
+        <Route path="/my-bookings" element={<MyBookings />} />
+        <Route path="/my-profile" element={<MyProfile />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/assets/assets.js
@@ -0,0 +1,100 @@
+import appointment_img from "./appointment_img.png";
+import header_img from "./header_img.png";
+import group_profiles from "./group_profiles.png";
+import profile_pic from "./profile_pic.png";
+import contact_image from "./contact_image.png";
+import about_image from "./about_image.png";
+import logo from "./logo.svg";
+import dropdown_icon from "./dropdown_icon.svg";
+import menu_icon from "./menu_icon.svg";
+import cross_icon from "./cross_icon.png";
+import chats_icon from "./chats_icon.svg";
+import verified_icon from "./verified_icon.svg";
+import arrow_icon from "./arrow_icon.svg";
+import info_icon from "./info_icon.svg";
+import upload_icon from "./upload_icon.png";
+import stripe_logo from "./stripe_logo.png";
+import razorpay_logo from "./razorpay_logo.png";
+import coach1 from "./TJheadshot.webp";
+import coach2 from "./GTheadshot.webp";
+import coach3 from "./PB.webp";
+import coach4 from "./MB.webp";
+import coach5 from "./TBENCH.webp";
+import coach6 from "./MRheadshot.webp";
+
+export const assets = {
+  appointment_img,
+  header_img,
+  group_profiles,
+  logo,
+  chats_icon,
+  verified_icon,
+  info_icon,
+  profile_pic,
+  arrow_icon,
+  contact_image,
+  about_image,
+  menu_icon,
+  cross_icon,
+  dropdown_icon,
+  upload_icon,
+  stripe_logo,
+  razorpay_logo,
+};
+
+export const doctors = [
+  {
+    _id: "coach1",
+    name: "Tom Jackson",
+    image: coach1,
+    experience: "x Years",
+    about:
+      "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.",
+    fees: 150,
+  },
+  {
+    _id: "coach2",
+    name: "Gregor Tilch",
+    image: coach2,
+    experience: "x Years",
+    about:
+      "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.",
+    fees: 150,
+  },
+  {
+    _id: "coach3",
+    name: "Peter Bakker",
+    image: coach3,
+    experience: "x Years",
+    about:
+      "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.",
+    fees: 150,
+  },
+  {
+    _id: "coach4",
+    name: "Michael Byrnes",
+    image: coach4,
+    experience: "x Years",
+    about:
+      "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.",
+    fees: 150,
+  },
+  {
+    _id: "coach5",
+    name: "Charles Arias",
+    image: coach5,
+    experience: "x Years",
+    about:
+      "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.",
+    fees: 150,
+  },
+  {
+    _id: "coach1",
+    name: "Marcel Rauch",
+    image: coach6,
+    experience: "4 Years",
+    about:
+      "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.",
+    fees: 150,
+  },
+];
