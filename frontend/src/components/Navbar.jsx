2a96ca43f41c9e5b01833eea41d78b7dcd9c9798
Nattan Aguia <nattan23@msn.com>
2025-08-16 19:00:44 -0400

    adds on the Navbar component to make it responsive in all devices, developed my-bookings component

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,6 +1,7 @@
-import { NavLink, useNavigate } from "react-router-dom";
+import { Link, NavLink, useNavigate } from "react-router-dom";
 import { assets } from "../assets/assets";
 import { useState } from "react";
+import { TbRuler2 } from "react-icons/tb";
 
 const Navbar = () => {
   const navigate = useNavigate();
@@ -65,6 +66,41 @@ const Navbar = () => {
             Create Account
           </button>
         )}
+        <img
+          onClick={() => setShowMenu(TbRuler2)}
+          className="w-6 md:hidden"
+          src={assets.menu_icon}
+          alt=""
+        />
+        <div
+          className={`${
+            showMenu ? "fixed w-full" : "h-0 w-0"
+          } md:hidden right-0 top-0 bottom-0 z-20 overflow-hidden bg-white transition-all`}
+        >
+          <div className="flex items-center justify-between px-5 py-6 ">
+            <img className="w-36" src={assets.logo} alt="" />
+            <img
+              className="w-7"
+              onClick={() => setShowMenu(false)}
+              src={assets.cross_icon}
+              alt=""
+            />
+          </div>
+          <ul className="flex flex-col items-center gap-2 mt-5 px-5 text-lg font-medium">
+            <NavLink onClick={() => setShowMenu(false)} to="/">
+              <p className="px-4 py-2 rounded inline-block">HOME</p>
+            </NavLink>
+            <NavLink onClick={() => setShowMenu(false)} to="/coaches">
+              <p className="px-4 py-2 rounded inline-block">OUR COACHES</p>
+            </NavLink>
+            <NavLink onClick={() => setShowMenu(false)} to="/about">
+              <p className="px-4 py-2 rounded inline-block">ABOUT</p>
+            </NavLink>
+            <NavLink onClick={() => setShowMenu(false)} to="/contact">
+              <p className="px-4 py-2 rounded inline-block">CONTACT</p>
+            </NavLink>
+          </ul>
+        </div>
       </div>
     </div>
   );
--- frontend/src/pages/MyBookings.jsx
+++ frontend/src/pages/MyBookings.jsx
@@ -1,9 +1,33 @@
+import { useContext } from "react"
+import { AppContext } from "../context/AppContext"
 
 
 const MyBookings = () => {
+
+  const {coaches}=  useContext(AppContext)
+
   return (
     <div>
-      
+      <p className="pb-3 mt-12 font-medium text-zinc-700 border-b">My Bookings</p>
+      <div>
+        {coaches.slice(0,2).map((coach, index) => (
+          <div className="grid grid-cols-[1fr_2fr] gap-4 sm:flex sm:gap-6 py-2 border-b" key={index}>
+            <div>
+              <img className="w-32 bg-indigo-50" src={coach.image} alt="" />
+            </div>
+            <div className="flex-1 text-sm text-zinc-600">
+              <p className="text-neutral-800 font-semibold">{coach.name}</p>
+              <p className="text-neutral-700">{coach.subtitle}</p>
+              <p>{coach.fees}</p>
+              <p className="text-xs mt-1"><span className="text-sm text-neutral-700 font-medium">Date & Time:</span>25, August, 2025 | 8:00 AM</p>
+            </div>
+            <div></div>
+            <div className="flex flex-col gap-2 justify-end">
+              <button className="text-sm text-stone-500 text-center sm:min-w-48 py-2 border rounded hover:bg-primary hover:text-white transition-all duration-300">Pay Online</button><button className="text-sm text-stone-500 text-center sm:min-w-48 py-2 border rounded hover:bg-red-600 hover:text-white transition-all duration-300">Cancel Booking</button>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
