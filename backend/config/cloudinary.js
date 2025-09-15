e133abb87e2e7f8c40488d14d5385664eeec909a
Nattan Aguia <nattan23@msn.com>
2025-09-15 19:06:34 -0400

    changing name for .env variables

--- backend/config/cloudinary.js
+++ backend/config/cloudinary.js
@@ -3,7 +3,7 @@ import {v2 as cloudinary} from 'cloudinary'
 const connectCloudinay = async () => {
 
     cloudinary.config({
-        cloud_name:process.env.CLOUDINARI_NAME,
+        cloud_name:process.env.CLOUDINARY_NAME,
         api_key: process.env.CLOUDINARY_API_KEY,
         api_secret: process.env.CLOUDINARY_SECRET_KEY
     })
