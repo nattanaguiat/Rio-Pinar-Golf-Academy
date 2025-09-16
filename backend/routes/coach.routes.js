a5605ae1c34212fa5e0531cc0c5ba737096f3a3c
Nattan Aguia <nattan23@msn.com>
2025-09-16 18:28:57 -0400

    correcting the import for the coachRouter

--- backend/routes/coach.routes.js
+++ backend/routes/coach.routes.js
@@ -1,16 +1,25 @@
-import { Router } from 'express'
-import { bookingCancel, bookingComplete, bookingsCoach, coachDashboard, coachesList, coachProfile, loginCoach, updateCoachProfile } from '../controllers/coach.controller.js'
-import authCoach from '../middlewares/authCoach.js'
+import { Router } from "express";
+import {
+  bookingCancel,
+  bookingComplete,
+  bookingsCoach,
+  coachDashboard,
+  coachesList,
+  coachProfile,
+  loginCoach,
+  updateCoachProfile,
+} from "../controllers/coach.controller.js";
+import authCoach from "../middlewares/authCoach.js";
 
-const coachRouter = Router()
+const coachRouter = Router();
 
-coachRouter.get('/list', coachesList)
-coachRouter.post('/login', loginCoach)
-coachRouter.get('/bookings', authCoach, bookingsCoach)
-coachRouter.post('/complete-booking', authCoach, bookingComplete)
-coachRouter.post('/cancel-booking', authCoach, bookingCancel)
-coachRouter.get('/dashboard', authCoach, coachDashboard)
-coachRouter.get('/profile', authCoach, coachProfile)
-coachRouter.post('/update-profile', authCoach, updateCoachProfile)
+coachRouter.get("/list", coachesList);
+coachRouter.post("/login", loginCoach);
+coachRouter.get("/bookings", authCoach, bookingsCoach);
+coachRouter.post("/complete-booking", authCoach, bookingComplete);
+coachRouter.post("/cancel-booking", authCoach, bookingCancel);
+coachRouter.get("/dashboard", authCoach, coachDashboard);
+coachRouter.get("/profile", authCoach, coachProfile);
+coachRouter.post("/update-profile", authCoach, updateCoachProfile);
 
-export default coachRouter
+export default coachRouter;
